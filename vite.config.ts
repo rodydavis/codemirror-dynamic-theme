a5882731368933773f0f979041f1052b1941bad2
Rody Davis <rody.davis.jr@gmail.com>
2022-05-24 21:39:27 -0700

    updating

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { resolve } from "path";
 export default defineConfig({
   base: "/codemirror-dynamic-theme/",
   build: {
+    lib: {
+      entry: "src/code-window.ts",
+      formats: ["es"],
+    },
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
